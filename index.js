adc5bdf1380df55aa4f0aefb82e6d70a834f00dc
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-01-10 11:05:41 +0530

    Client-server interaction ready !

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ app.post('/api', async (req, res)=>{
 })
     
 // FOR DEVELOPMENT
-const port = 5000;
-app.listen(port, ()=>{
-  console.log(`Listening on port ${port}....`);
-})
+// const port = 5000;
+// app.listen(port, ()=>{
+//   console.log(`Listening on port ${port}....`);
+// })
 
 // FOR PRODUCION
-// const port = process.env.PORT || 5000;
-// app.listen(port,()=>{console.log(`Listening on port ${port}`);})
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{console.log(`Listening on port ${port}`);})
