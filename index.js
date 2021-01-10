eeb256da752fde39f7287d53003784f573275363
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2021-01-11 00:11:58 +0530

    Graphs added iteration #1

--- index.js
+++ index.js
@@ -58,11 +58,11 @@ app.post('/api', async (req, response)=>{
 
     
 // FOR DEVELOPMENT
-const port = 5000;
-app.listen(port, ()=>{
-  console.log(`Listening on port ${port}....`);
-})
+// const port = 5000;
+// app.listen(port, ()=>{
+//   console.log(`Listening on port ${port}....`);
+// })
 
 // FOR PRODUCION
-// const port = process.env.PORT || 5000;
-// app.listen(port,()=>{console.log(`Listening on port ${port}`);})
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{console.log(`Listening on port ${port}`);})
