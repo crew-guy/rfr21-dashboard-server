525abcb9d46ead11f764b4be4302e5b47a2d1705
ankit <69717009+crew-guy@users.noreply.github.com>
2021-02-03 00:28:52 +0530

    reconfiged pg db creds

--- index.js
+++ index.js
@@ -63,8 +63,8 @@ execute()
 // })
 
 // FOR PRODUCION
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
+let server = app.listen(process.env.PORT || 5000, function () {
+  let port = server.address().port;
   console.log("Express is working on port " + port);
 });
 
