161c4f01760d3bd0956f14e1a7566892c178f1a5
ankit <69717009+crew-guy@users.noreply.github.com>
2021-02-04 10:29:54 +0530

    trying server db connection

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ const client = new Client(
 
 client.connect()
 
-app.post('/test', async (req, res) =>
-{
-  const value = `test api is working`
-  res.send(value)
-})
+// app.post('/test', async (req, res) =>
+// {
+//   const value = `test api is working`
+//   res.send(value)
+// })
 
 app.post('/api', async (req, response) => {  
   console.log(req.body)
