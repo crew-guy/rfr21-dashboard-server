57ba7ffecdbb92da8207a95a14f586cc7c264e63
ankit <69717009+crew-guy@users.noreply.github.com>
2021-02-04 12:23:12 +0530

    trying a jugaad

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const path = require('path')
 require('dotenv').config()
 
-app.use(express.static(path.join(__dirname,'client/build')));
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json());
 
